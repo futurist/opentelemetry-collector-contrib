c525c09d7ea28e99625047c0c90b6f89d59f3d5d
futurist <26634873@qq.com>
2023-03-25 00:09:09 +0800

    feat: add receiver/prometheusremotewritereceiver

---
+++ receiver/prometheusremotewritereceiver/factory.go
@@ -0,0 +1,70 @@
+// Copyright  The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package prometheusremotewritereceiver
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config/configgrpc"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/confignet"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+)
+
+const (
+	typeStr              = "prometheusremotewrite"
+	stability            = component.StabilityLevelDevelopment
+	defaultTimeThreshold = 24
+
+	defaultGRPCBindEndpoint = "0.0.0.0:19290"
+	defaultHTTPBindEndpoint = "0.0.0.0:19291"
+)
+
+// NewFactory return a new receiver.Factory for receiver.
+func NewFactory() receiver.Factory {
+	return receiver.NewFactory(
+		typeStr,
+		createDefaultConfig,
+		receiver.WithMetrics(createMetricsReceiver, stability))
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{
+		TimeThreshold: defaultTimeThreshold,
+		Protocols: Protocols{
+			GRPC: &configgrpc.GRPCServerSettings{
+				NetAddr: confignet.NetAddr{
+					Endpoint:  defaultGRPCBindEndpoint,
+					Transport: "tcp",
+				},
+			},
+			HTTP: &confighttp.HTTPServerSettings{
+				Endpoint: defaultHTTPBindEndpoint,
+			},
+		},
+	}
+}
+
+func createMetricsReceiver(
+	_ context.Context,
+	settings receiver.CreateSettings,
+	cfg component.Config,
+	consumer consumer.Metrics,
+) (receiver.Metrics, error) {
+	rCfg := cfg.(*Config)
+	return NewReceiver(settings, rCfg, consumer)
+}
